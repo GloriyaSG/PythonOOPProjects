50f3a087ae2c30b90965b76d07804a857f9956c6
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-07-17 13:34:26 +0300

    Added more solutions

--- class_and_static_methods_exercise/document_management/project/category.py
+++ class_and_static_methods_exercise/document_management/project/category.py
@@ -1,2 +1,11 @@
 class Category:
-    pass
+
+    def __init__(self, id: int, name: str):
+        self.id = id
+        self.name = name
+
+    def edit(self, new_name: str) -> None:
+        self.name = new_name
+
+    def __repr__(self) -> str:
+        return f"Category {self.id}: {self.name}"
--- class_and_static_methods_exercise/document_management/project/storage.py
+++ class_and_static_methods_exercise/document_management/project/storage.py
@@ -1,2 +1,32 @@
+from typing import List
+
+from project.category import Category
+from project.document import Document
+from project.topic import Topic
+
+
 class Storage:
-    pass
+
+    def __init__(self):
+        self.categories: List[Category] = []
+        self.topics: List[Topic] = []
+        self.documents: List[Document] = []
+
+    def add_category(self, category:Category):
+        if category not in self.categories:
+            self.categories.append(category)
+
+    def add_topic(self, topic:Topic):
+        if topic not in self.topics:
+            self.topics.append(topic)
+
+    def add_document(self, document: Document):
+        if document not in self.documents:
+            self.documents.append(document)
+
+    def edit_category(self, category_id: int, new_name:str):
+        category = [category for category in self.categories if category_id == category.id][0]
+        category.name = new_name
+
+    def edit_topic(self, topic_id: int, new_topic: str, new_storage_folder: str):
+        pass
