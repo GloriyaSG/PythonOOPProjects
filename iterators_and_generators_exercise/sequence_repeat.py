6798904da36dd49b867515d903ee0fd80783a111
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-07-24 16:31:54 +0300

    Added more solutions

--- iterators_and_generators_exercise/sequence_repeat.py
+++ iterators_and_generators_exercise/sequence_repeat.py
@@ -1,11 +1,23 @@
 class sequence_repeat:
 
     def __init__(self, sequence, number):
-        self.sequence = sequence
+        self.sequence = list(sequence)
         self.number = number
+        self.index = -1
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if len(self.sequence) < self.number:
+        self.index += 1
+        if self.number == 0:
+            raise StopIteration
+        if self.index >= len(self.sequence):
+            self.index = 0
+        self.number -= 1
+        return self.sequence[self.index]
+
+
+result = sequence_repeat('I Love Python', 3)
+for item in result:
+    print(item, end='')
