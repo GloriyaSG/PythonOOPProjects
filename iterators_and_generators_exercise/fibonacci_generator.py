7d36e5c4d0771b2bc15dbd75c85eb274279556c1
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-07-25 14:25:00 +0300

    Added more solutions

---
+++ iterators_and_generators_exercise/fibonacci_generator.py
@@ -0,0 +1,13 @@
+def fibonacci():
+    first, second = 0, 1
+    while True:
+        yield first
+        first, second = second, first + second
+
+
+generator = fibonacci()
+for i in range(5):
+    print(next(generator))
+
+
+
